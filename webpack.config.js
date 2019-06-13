bde350cd12b78f243d1124c5c343f42c5c84d85b
ydx731027521 <ydx731027521@163.com>
2019-06-14 00:19:36 +0800

    react脚手架

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: process.env === 'development' ? "cheap-module-eval-source-map" : "cheap-module-source-map",
+  module:{
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [
+          {loader: 'babel-loader'}
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins:[
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html', // 最终创建的文件名
+      template: path.join(__dirname, 'src/template.html') // 指定模板路径
+    })
+  ],
+  devServer: {
+    hot: true,
+    contentBase: path.join(__dirname, "./dist"),
+    host: "0.0.0.0", // 可以使用手机访问
+    port: 8080,
+    historyApiFallback: true, // 该选项的作用所有的404都连接到index.html
+    proxy: {
+      // 代理到后端的服务地址，会拦截所有以api开头的请求地址
+      "/api": "http://localhost:3000"
+    }
+  }
+}
